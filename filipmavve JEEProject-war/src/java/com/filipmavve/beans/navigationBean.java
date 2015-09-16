030e2cbbed7e4fe65de76ac87b1cd6e6ff7f7e4f
Filip Ferm <ferm.filip@gmail.com>
2015-09-16 16:13:14 +0200

    Template added

---
+++ filipmavve JEEProject-war/src/java/com/filipmavve/beans/navigationBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.filipmavve.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author Filip
+ */
+@ManagedBean
+@ViewScoped
+public class navigationBean {
+    
+    String page = "hello.xhtml";
+
+    /**
+     * Creates a new instance of navigationBean
+     */
+    public navigationBean() {
+    }
+    
+    public void goToPage(ActionEvent event) {
+        String id = event.getComponent().getId();
+        switch (id) {
+            case "StudentPage": page = "StudentPage.xhtml";
+                break;
+            case "CoursePage": page = "CoursePage.xhtml";
+                break;
+            default: page = "hello.xhtml";
+                break;
+        }
+    }
+
+    public String getPage() {
+        return page;
+    }
+    
+    
+}
